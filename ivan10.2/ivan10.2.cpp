e77fe9a97d896caa1b6371d1f57cd6aa0c9ef67a
sedelnikova <54905384+sedelnikova@users.noreply.github.com>
2019-09-30 13:04:45 +0300

    Добавьте файлы проекта.

---
+++ ivan10.2/ivan10.2.cpp
@@ -0,0 +1,23 @@
+﻿// ivan 10.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//2. Дано целое число N (> 0). Найти произведение 1.1 • 1.2 • 1.3 • . . . (N сомножителей).
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double  s;
+	int n;
+	cout << "введите число N (N>0)" << endl;
+	cin >> n;
+	s = 1.0;
+	for (double i = 1.1, j = 1.0; j <= n; j++, i = i + 0.1)
+	{
+		s = s * i;
+	}
+
+	cout << " произведение =  " << s << endl;
+	return 0;
+}
